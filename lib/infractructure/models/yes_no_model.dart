87de34bf817ba778489653b11b9464613003c2e9
Miguel Herrera <herreraalcantarmiguelangel@gmail.com>
2023-05-25 11:16:52 +0200

    first commit

---
+++ lib/infractructure/models/yes_no_model.dart
@@ -0,0 +1,45 @@
+// To parse this JSON data, do
+//
+//     final yesNoModel = yesNoModelFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:yes_no_app/domain/entities/message.dart';
+
+YesNoModel yesNoModelFromJson(String str) =>
+    YesNoModel.fromJsonMap(json.decode(str));
+
+String yesNoModelToJson(YesNoModel data) => json.encode(data.toJson());
+
+class YesNoModel {
+  String answer;
+  bool forced;
+  String image;
+
+  YesNoModel({
+    required this.answer,
+    required this.forced,
+    required this.image,
+  });
+
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+      text: answer == 'yes'
+          ? 'Oui'
+          : answer == 'maybe'
+              ? 'Peut-être'
+              : 'Non',
+      fromWho: FromWho.hers,
+      imageUrl: image);
+}
---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://img.freepik.com/foto-gratis/amor-romance-perforado-corazon-papel_53876-87.jpg'),
+          ),
+        ),
+        title: const Text('Mon amour <3'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: chatProvider.chatScrollController,
+                itemCount: chatProvider.messageList.length,
+                itemBuilder: (context, index) {
+                  final message = chatProvider.messageList[index];
+                  return message.fromWho == FromWho.hers
+                      ? HerMessageBubble(message: message)
+                      : MyMessageBubble(
+                          message: message,
+                        );
+                },
+              ),
+            ),
+            // Caja de texto
+            MessageFieldBox(onValue: chatProvider.sendMessage),
+          ],
+        ),
+      ),
+    );
+  }
+}
